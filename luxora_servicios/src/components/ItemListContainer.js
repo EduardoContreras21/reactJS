5093392c2bbbeb31f5f1558ee59891fffc3bc49a
Eduardo Contreras <libec21@hotmail.com>
2022-06-26 22:22:53 -0500

    ItemCount

--- luxora_servicios/src/components/ItemListContainer.js
+++ luxora_servicios/src/components/ItemListContainer.js
@@ -1,8 +1,17 @@
+import React from 'react'
+import ItemCount from './ItemCount'
+
 /*Item List Container*/ 
 const ItemListContainer = () => {
+    /*OnADD*/ 
+    const onAdd =() =>{
+        alert('Gracias por tu elección')}
     return(
+        <>
         <h2 style={styles.titulos}>LISTA DE SERVICIOS DE LUXORA</h2>
-    )
+        <ItemCount initial={0} stock={3} onAdd={onAdd}/>
+        </>
+        )
 }
 
 const styles ={
