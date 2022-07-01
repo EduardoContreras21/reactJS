24ac13aa22e47e514ad065cf137c77266e4f60d5
Eduardo Contreras <libec21@hotmail.com>
2022-06-30 23:52:43 -0500

    maps&promises

---
+++ src/components/ItemCount.js
@@ -0,0 +1,30 @@
+import { useState} from "react"
+
+const ItemCount = ({initial=0, stock, onAdd}) => {
+    const [contador, setContador] = useState(0);
+    
+    /*SUMAR*/ 
+    const agregar = () =>{
+        contador<stock && setContador(contador+1);
+    }
+    /*RESTAR*/ 
+    const restar = () =>{
+        contador>0 && setContador(contador-1)
+    } 
+    /*RESET*/ 
+    const reset = () =>{
+        setContador(initial)
+    }
+    return(
+        <>
+        <h2>¿Cuantos servicios necesitas?</h2>
+        <h2>{contador}</h2>
+        <button onClick={agregar}>+</button>
+        <button onClick={restar}>-</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={onAdd}>Elegir</button>
+        </>
+    )
+}
+
+export default ItemCount
