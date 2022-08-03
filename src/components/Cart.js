4f96351f725050460885dea090d18118c6977b0d
Eduardo Contreras <libec21@hotmail.com>
2022-08-03 18:00:13 -0500

    proyectoFinal2

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,33 +1,65 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { contexto } from '../Context/CartContext';
 import { Link } from 'react-router-dom'
+import { db } from '../Firebase/Firebase'
+import { collection, addDoc, serverTimestamp, doc, updateDoc } from 'firebase/firestore';
+import Formulario from './Formulario';
 
 const Cart =() => {
   const { servCarrito, removeServicio, clearServicio, total } = useContext(contexto);
+  const [idVenta, setIdVenta] = useState("")
 
-  return (
+  const comprador = {
+    Nombre: '',
+    Apellido: '',
+    Celular: '',
+}
+
+const finalizarCompra = () =>{
+  const ventasCollection = collection (db, 'ventas');
+  addDoc(ventasCollection,{
+    comprador,
+    items:contexto,
+    date: serverTimestamp(),
+    total: total,
+  })
+  .then((result)=>{
+    setIdVenta(result.id)
+  })
+
+  const updateCollection = doc(db, "servicios") 
+  updateDoc(updateCollection)
+}
+return (
     <>
-    <h2>Resumen de compra</h2>
+    <h3 style={styles.titulo}>RESUMEN DE COMPRA</h3>
     {servCarrito.length === 0 ?
     <div>
-      <h2>Por favor agrega un servicio</h2>
+      <h4>Por favor agrega un servicio</h4>
       <Link to='/'><button className= 'btn btn-primary'>Agregar servicio</button></Link> 
     </div>
     :
     <>
     {servCarrito.map(c => 
       <div>
-        <h2 key={c.id}> {c.title} <button onClick={() => removeServicio(c.id)} className= 'btn btn-primary'>Quitar servicio</button></h2>
+        <h4 key={c.id}> {c.title} <button onClick={() => removeServicio(c.id)} className= 'btn btn-primary'>Quitar servicio</button></h4>
         <span>Cantidad: {c.cantidad}</span><br></br>
         <span>${c.cantidad * c.precio}</span><br></br>
       </div>
     )}
         <div> Total de la compra: ${total()} </div>
-        <button onClick={clearServicio} className= 'btn btn-primary'>No quiero agendar</button>
-        <Link to='/Formulario'><button className= 'btn btn-primary'>Comprar</button></Link>
+        <button onClick={clearServicio} className= 'btn btn-primary'>No quiero agendar</button><br></br><br></br>
+        <h3 style={styles.titulo}>DATOS DEL COMPRADOR</h3>
+        <Formulario />
+        <button className= 'btn btn-primary'type="submit" onClick={finalizarCompra}>Comprar</button>
     </>}
     </>
   )
+  }
+  const styles ={
+    titulo:{
+      textAlign: 'center',
+      }
   }
 
 export default Cart
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,75 +1,25 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import Form from 'react-bootstrap/Form';
-import { contexto } from '../Context/CartContext';
-import { db } from '../Firebase/Firebase';
-import { collection, addDoc } from 'firebase/firestore';
-import { Link } from 'react-router-dom';
 
-export const Formulario =() => {
-  const { carrito, clearServicio, total } = useContext(contexto)
-    const [idOrden, setIdOrden] = useState()
-    const [loading, setLoading] = useState(false)
-    const [comprador, setComprador] = useState({
-        Nombre: '',
-        Apellido: '',
-        Celular: '',
-    })
-    const { Nombre, Apellido, Celular } = comprador
-    const Input = (e) => {
-        setComprador(({
-            ...comprador,
-            [e.target.name]:e.target.value
-        }))
-    }
-    const generarPedido = async (data) => {
-        try {
-            const coleccion = collection(db, 'ventas')
-            const order = await addDoc(coleccion, data)
-            setIdOrden(order.id)
-            clearServicio()
-        } catch (error) {
-            <p>error</p>
-        }
-    }
-    const enviar = (e) => {
-        e.preventDefault()
-        const dia = new Date()
-        const items = carrito.map(e => { return { Id:e.id, Titulo: e.title, Precio: e.precio, disponibilidad: e.disponibilidad } })
-        const totalCompra = total()
-        const data = { comprador, dia, items, totalCompra }
-        generarPedido(data)
-    }
+export function Formulario ()  {        
   return (
     <>
-    {loading ? <p>cargando</p>
-                : (!idOrden && <div>
-    <Form onSubmit={enviar}> 
+        <Form > 
         <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Nombre</Form.Label>
-        <Form.Control type="name" onChange={Input} placeholder="Nombre" value={Nombre} required />
+        <Form.Control type="name" placeholder="Nombre"  required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicLastName">
         <Form.Label>Apellidos</Form.Label>
-        <Form.Control type="text" onChange={Input} placeholder="Apellido" value={Apellido} required />
+        <Form.Control type="text" placeholder="Apellido" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPhone">
         <Form.Label>Numero de celular</Form.Label>
-        <Form.Control type="number" onChange={Input} placeholder="Celular" value={Celular} required />
+        <Form.Control type="number" placeholder="Celular" required />
         </Form.Group>
-          <button className='btn btn-primary' type="submit">
-            Finalizar Compra
-          </button>
-    </Form></div>
-  )}
-  {idOrden&&(
-  <div>
-  <h3>{`Gracias por tu Compra ${Nombre} ${Apellido}`}</h3>
-  <p>{`ID de compra ${idOrden}`}</p>
-  <div ><Link to='/' >Realizar otra compra </Link></div>
-  </div>
-    )}
-    </>
-    )
-  }
+        </Form>
+  </ >
+  )
+}
 
 export default Formulario;
